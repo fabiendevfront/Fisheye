25703de4a57b7524f18c61f511cce3039533c5e2
Fabien Dev <fabiendevfront@gmail.com>
2022-05-24 17:47:45 +0200

    Reformating indent for existing JS files, config indent in eslint

--- assets/js/factories/photographer.js
+++ assets/js/factories/photographer.js
@@ -1,17 +1,19 @@
+// eslint-disable-next-line no-unused-vars
 function photographerFactory(data) {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        const h2 = document.createElement("h2");
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, getUserCardDOM };
 }
