4afdf53647152f96debc5fd5d9ea7d7675dc6db8
Fabien Dev <fabiendevfront@gmail.com>
2022-06-04 23:42:16 +0200

    Modification of factory function photographerFactory for return all data of photographer, code refactoring: arrows functions

--- src/scripts/factories/PhotographerFactory.js
+++ src/scripts/factories/PhotographerFactory.js
@@ -1,18 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-function photographerFactory(data) {
-    const { name, portrait } = data;
+const photographerFactory = (data) => {
+    const { id, name, city, country, tagline, price, portrait } = data;
+    const picture = `assets/images/photographers/${portrait}`;
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
 
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( "article" );
-        const img = document.createElement( "img" );
+    const getUserCardDOM = () => {
         img.setAttribute("src", picture);
-        const h2 = document.createElement( "h2" );
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM };
-}
+        return article;
+    };
+
+    return { id, name, city, country, tagline, price, picture, getUserCardDOM };
+};
