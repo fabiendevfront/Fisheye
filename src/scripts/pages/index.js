27255d752fd600aec77fc61472e528a5e811b96c
Fabien Dev <fabiendevfront@gmail.com>
2022-06-04 23:32:01 +0200

    JSON data recovery with async function and fetch, refactoring code: arrow functions, add comments.

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,44 +1,48 @@
-async function getPhotographers() {
-    // Penser à remplacer par les données récupérées dans le json
-    const photographers = [
-        {
-            "name": "Ma data test",
-            "id": 1,
-            "city": "Paris",
-            "country": "France",
-            "tagline": "Ceci est ma data test",
-            "price": 400,
-            "portrait": "account.png"
-        },
-        {
-            "name": "Autre data test",
-            "id": 2,
-            "city": "Londres",
-            "country": "UK",
-            "tagline": "Ceci est ma data test 2",
-            "price": 500,
-            "portrait": "account.png"
-        },
-    ];
-    // et bien retourner le tableau photographers seulement une fois
-    return ({
-        photographers: [...photographers, ...photographers, ...photographers]});
-}
+// Fonction qui récupère les données JSON avec Fetch asynchrone
+const getPhotographers = async () => {
+    // 1ere méthode
+    try {
+        let response = await fetch("../../src/data/photographers.json");
+        let data = await response.json();
+        return ({photographers: [...data.photographers]});
+    } catch(error) {
+        throw new Error("Erreur à la récupération des données : ", error);
+    }
 
-async function displayData(photographers) {
+    // 2ème méthode
+    // const photographers = await fetch("../../src/data/photographers.json")
+    //     .then((response) => response.json())
+    //     .then((data) => {
+    //         return data.photographers;
+    //     })
+    //     .catch((error) => {
+    //         throw new Error("Erreur à la récupération des données : ", error);
+    //     });
+    // return ({photographers: [...photographers]});
+};
+
+// Affiche les données des photographes
+const displayData = async (photographers) => {
+    // Noeud HTML dans lequel on insere la liste des photographes
     const photographersSection = document.querySelector(".photographer_section");
-
+    // On parcours les photographes
     photographers.forEach((photographer) => {
+        // Creer un objet par photographe avec la factory function avec toutes ses infos
+        // eslint-disable-next-line no-undef
         const photographerModel = photographerFactory(photographer);
+        // Utilise "getUserCardDOM" pour générer le HTML de la card
         const userCardDOM = photographerModel.getUserCardDOM();
+        // Ajoute la card au DOM
         photographersSection.appendChild(userCardDOM);
     });
-}
+};
 
-async function init() {
+// Fonction qui initialise l'App en récuprérant les données du JSON et en affichant les cards photographes
+const init = async () => {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
+    // Affiches les photographes
     displayData(photographers);
-}
-
+};
+// Initialise l'App
 init();
