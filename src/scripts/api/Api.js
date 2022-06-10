65e01c87b2f9a0c949abfb5404ea4eec0ac415af
Fabien Dev <fabiendevfront@gmail.com>
2022-06-10 15:28:05 +0200

    Added a latency in the getPhotographersData function

--- src/scripts/api/Api.js
+++ src/scripts/api/Api.js
@@ -8,7 +8,12 @@ class Api {
     async getPhotographersData() {
         try {
             const response = await fetch(this.url);
+            console.log(response);
+            const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+            await sleep(2000);
             const data = await response.json();
+            console.log(data);
+            console.log(data.photographers);
             return data.photographers;
         } catch(error) {
             throw new Error("Erreur à la récupération des données : ", error);
